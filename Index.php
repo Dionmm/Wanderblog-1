e492e8bed9bdfd6f091d6b3884fca0a12b4a748a
RmK9 <th3rmk@gmail.com>
2015-10-19 14:47:41 +0100

    testing sql connection configs

--- Index.php
+++ Index.php
@@ -13,5 +13,26 @@
     echo "Shaggers";
     ?>
 
+<?php
+/*
+    require_once 'config.php';
+
+    try {
+        $oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
+        $oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $oStmt = $oConn->prepare('SELECT data FROM `hello_world`');
+        $oResult = $oStmt->fetchAll();
+
+        foreach ($oResult as $aRow) {
+            print_r($aRow['data']);
+        }
+
+    } catch(PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+    }
+
+*/?>
+
 </body>
 </html>
