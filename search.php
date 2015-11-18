964735749bb8e1ce704d1adf71560a09396daa5c
RmK9 <th3rmk@gmail.com>
2015-11-18 02:26:58 +0000

    Search function now implemented and searches by Keywords.

--- search.php
+++ search.php
@@ -1,21 +1,29 @@
 <?php
 
-try {
+require_once 'functions.php';
 
-    $query = $_POST["query"];
+    try {
+        $oConn = loginToDB(); //Login to DB (Functions.php)
 
+        $searchedQuery = $_POST["query"];
 
-    //Templating
-    require_once 'vendor/autoload.php';
-    $loader = new Twig_Loader_Filesystem('views');
-    $twig = new Twig_environment($loader);
-    $template = $twig->loadTemplate('search.html');
+        //Prepare statement, substitute :username with username field input
+        $query = $oConn->prepare("SELECT a.* FROM Adventures a, Keywords k WHERE a.PostID = k.PostID AND k.Keyword = '$searchedQuery'");
+        $query->execute();
+        $rows = $query->fetchAll(PDO::FETCH_ASSOC);
 
-    //Return the template specified above with the following variables filled in
-    echo $template->render(array(
-        'query' => $query,
-    ));
+        //Templating
+        require_once 'vendor/autoload.php';
+        $loader = new Twig_Loader_Filesystem('views');
+        $twig = new Twig_environment($loader);
+        $template = $twig->loadTemplate('search.html');
 
-} catch (Exception $e) {
-    die ('ERROR: ' . $e->getMessage());
-}
+        //Return the template specified above with the following variables filled in
+        echo $template->render(array(
+            'query' => $searchedQuery,
+            'adventures' => $rows
+        ));
+
+    } catch (PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+    }
