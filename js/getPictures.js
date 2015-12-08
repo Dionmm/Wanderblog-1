a44fa3c30e56c41ef3b55cf20ce7cf5fafca1a87
GuthrieS <sean@greycloud.co.uk>
2015-12-08 16:13:56 +0000

    Added in script to get the pictures for the adventure

---
+++ js/getPictures.js
@@ -0,0 +1,55 @@
+/**
+ * Created by Sean on 12/8/2015.
+ */
+window.onload = function() {
+
+    $("#preview").html("");
+
+    var qrStr = window.location.search;
+    if (qrStr) {
+        qrStr = qrStr.split("?")[1].split("=")[1];
+
+        //Is an adventure id
+        if(qrStr.length == 5){
+            var adventureId = qrStr;
+            var pictureCount = 0;
+
+            $.ajax({
+                type: "POST",
+                url: 'picture.php',
+                data: {adventureId:adventureId},
+                dataType: 'json',
+                success: function(data){
+                    for(var key in data) {
+
+                            var adventurePicture = data[key];
+
+                            pictureCount++;
+
+                            var pictureId = "picture" + pictureCount;
+
+                            var li = document.createElement("li");
+                            li.classList.add("col-lg-2");
+                            li.classList.add("col-md-2");
+                            li.classList.add("col-sm-3");
+                            li.classList.add("col-xs-4");
+                            li.id = pictureId
+
+                            var img = document.createElement("img");
+                            img.classList.add("img-thumbnail");
+                            img.src = adventurePicture;
+
+                            var preview = document.getElementById("preview");
+                            preview.appendChild(li); // Assuming that "preview" is the div output where the content will be displayed.
+
+                            var newPicture = document.getElementById(pictureId)
+                            newPicture.appendChild(img);
+                    }
+                },error: function(error){
+                    console.log("Error: " + JSON.stringify(error));
+                }
+            });
+        }
+    }
+}
+
---
+++ picture.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sean
+ * Date: 12/8/2015
+ * Time: 4:05 PM
+ */
+require_once 'functions.php'; //Grabs any extra functions
+session_start();
+
+try {
+    $postId = $_POST['adventureId'];
+
+    if($postId){
+
+        $oConn = loginToDB();
+
+        $pictures = array();
+
+        $sql_query = "SELECT * FROM pictures WHERE PostID = '$postId'";
+
+        $result = $oConn->query($sql_query);
+
+        while($row = $result->fetch_array()){
+            $pictures[] = $row["Path"];
+        }
+
+        echo json_encode($pictures);
+
+    }else{
+        echo 'Adventure not found';
+    }
+}catch (PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+} finally{
+    $oConn = null;
+}
