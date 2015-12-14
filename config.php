e7f262577965dd988243d0ece0185e24b1d6e062
dion <dion@macintyres.me.uk>
2015-12-14 15:21:08 +0000

    adding little p

--- config.php
+++ config.php
@@ -1,12 +1,6 @@
 <?php
 // Replace with your own
-$sUsername = 'bc1c13d41416a4';
-$sPassword = 'ffd21406';
-$sHost = 'eu-cdbr-azure-west-c.cloudapp.net:3306';
-$sDb = 'one-wanderblogDB';
-
-// Second DB (in case max_connections error occur, or first one is sluggish) -- SCHEMA COPIED ON 13/11/2015
-/*$sUsername = 'bb569b1ba9f9d0';
-$sPassword = 'a4813f00';
-$sHost = 'eu-cdbr-azure-west-c.cloudapp.net:3306';
-$sDb = 'one-wanderblogDB2';*/
+$sUsername = 'root';
+$sPassword = 'root';
+$sHost = 'localhost';
+$sDb = 'wanderblog';
